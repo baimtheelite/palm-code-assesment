3776f2ee7a6711eee526161050709ef004427c6b
Ibrahim Ahmad <ibrahim.ahmad@digitalsiber.id>
2025-06-30 21:26:50 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             // PermissionSeeder::class,
-            RoleSeeder::class,
+            // RoleSeeder::class,
             UserSeeder::class,
             PageSeeder::class,
         ]);
